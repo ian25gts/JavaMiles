2a8f728fbff159b9a114d10cd5ce4dd2fadb7a8b
Ivanov Alexey <ian25@rambler.ru>
2021-12-24 09:39:50 +0300

    ivanov first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 330;
+        int priceBonus = 20;
+
+        int bonusMiles = ticketPrice / priceBonus;
+        System.out.println("Ваши бонусные мили: " + bonusMiles);
+    }
+}
